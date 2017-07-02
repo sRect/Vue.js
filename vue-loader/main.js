ffaefd4dc86dbfae59a8164df5a8c7abb1e0a74a
sRect <srect2017@gmail.com>
2017-07-02 16:43:24 +0800

    add vue-loader demo

---
+++ vue-loader/main.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: xiaofang
+* @Date:   2017-07-02 16:36:57
+* @Last Modified by:   xiaofang
+* @Last Modified time: 2017-07-02 16:37:24
+*/
+
+'use strict';
---
+++ vue-loader/webpack.config.js
@@ -0,0 +1,8 @@
+/* 
+* @Author: xiaofang
+* @Date:   2017-07-02 16:37:36
+* @Last Modified by:   xiaofang
+* @Last Modified time: 2017-07-02 16:37:38
+*/
+
+'use strict';
