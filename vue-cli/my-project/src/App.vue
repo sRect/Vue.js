63cb82b351634d1fd33429573be23faf49d3cbb5
sRect <fcqun@sina.cn>
2017-07-04 16:20:49 +0800

    add vue-cli demo

---
+++ vue-cli/my-project/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>hello vue</h1>
+    <p>{{msg}}</p>
+    <input type="button" value="改变" @click="change">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    change() {
+      this.msg = this.msg.split('').reverse().join('');
+    }
+  }
+}
+</script>
+
+<style>
+  body{
+    background-color: #f0f0f0;
+  }
+</style>
