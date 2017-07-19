006104b395be7648f0095f5ebf56a1d3a1b41aff
sRect <fcqun@sina.cn>
2017-07-19 15:28:33 +0800

    添加axios

--- vue2.0/vue2.0-demo/src/components/Button.vue
+++ vue2.0/vue2.0-demo/src/components/Button.vue
@@ -3,6 +3,8 @@
 		<el-button @click="change">默认按钮</el-button>
 		<el-button type="primary">主要按钮</el-button>
 		<el-button type="text">文字按钮</el-button>
+
+		<el-button plain @click="get">可自动关闭</el-button>
 		<div>
 			<span class="text" v-text="msg"></span>
 		</div>
@@ -10,15 +12,34 @@
 </template>
 
 <script>
+	import axios from 'axios'
+
 	export default{
 		data: () => {
 			return {
-				msg: 'default info'
+				msg: 'default info',
+				getMsg: ''
 			}
 		},
 		methods: {
 			change: function(){
 				this.msg = "welome element-ui"
+			},
+			get(){
+				var self = this;
+				axios.get('http://localhost:8080/src/data.txt')
+					.then((res) => {
+						const h = this.$createElement;
+
+				        this.$notify.success({
+				          title: '注意',
+				          message: h('i', { style: 'color: teal'}, ''+res.data+'')
+				        });
+					})
+					.catch((err) => {
+						console.log(err)
+					})
+				
 			}
 		},
 		mounted: function(){
