030a13f47aa63a9e70f8e6fca685f0f774cc3844
sRect <fcqun@sina.cn>
2017-07-21 15:59:08 +0800

    添加通讯录模板

--- vue2.0/mint-ui-demo/src/components/List.vue
+++ vue2.0/mint-ui-demo/src/components/List.vue
@@ -1,18 +1,8 @@
 <template>
 	<div id="list">
 		<mt-index-list>
-			<mt-index-section index="A">
-				<mt-cell title="Aaron"></mt-cell>
-				<mt-cell title="Alden"></mt-cell>
-				<mt-cell title="Austin"></mt-cell>
-			</mt-index-section>
-			<mt-index-section index="B">
-				<mt-cell title="Baldwin"></mt-cell>
-				<mt-cell title="Braden"></mt-cell>
-			</mt-index-section>
-			<mt-index-section index="Z">
-				<mt-cell title="Zack"></mt-cell>
-				<mt-cell title="Zane"></mt-cell>
+			<mt-index-section v-for="(val,fatherindex) in arr" :index="val.index" :key="fatherindex">
+				<mt-cell v-for="(item,childindex) in val.data" :title="item" :key="childindex" @click.stop.prevent.native="get(item)"></mt-cell>
 			</mt-index-section>
 		</mt-index-list>
 	</div>
@@ -22,7 +12,20 @@
 	export default {
 		data(){
 			return {
-				
+				arr: [
+					{"index":"A","data":["Addd","Addf","Apple","Adksjdf"]},
+					{"index":"X","data":["Xddd","Xddf","Xpple","Xdksjdf"]},
+					{"index":"S","data":["Sddd","Sddf","Spple","Sdksjdf"]},
+					{"index":"G","data":["Gddd","Gddf","Gpple","Gdksjdf"]},
+					{"index":"U","data":["Uddd","Uddf","Upple","Udksjdf"]},
+					{"index":"J","data":["JJdd","Jddf","Jpple","Jdksjdf"]},
+					{"index":"M","data":["Mddd","Mddf","Mpple","Mdksjdf"]}
+				]
+			}
+		},
+		methods: {
+			get(data){
+				alert(data)
 			}
 		}
 	}
