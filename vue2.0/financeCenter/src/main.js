4374d9fcfba5884fa2e8b89f18ac31240d49a2f3
sRect <fcqun@sina.cn>
2017-07-25 14:51:07 +0800

    完成首页布局

--- vue2.0/financeCenter/src/main.js
+++ vue2.0/financeCenter/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import {Table,TableColumn} from 'element-ui'
+import {Table,TableColumn,Input,InputNumber,Button,DatePicker,Pagination} from 'element-ui'
 import App from './App.vue'
 
 import $ from 'jquery'
@@ -7,6 +7,11 @@ import './assets/js/common2.js'
 
 Vue.use(Table)
 Vue.use(TableColumn)
+Vue.use(Input)
+Vue.use(InputNumber)
+Vue.use(Button)
+Vue.use(DatePicker)
+Vue.use(Pagination)
 
 
 new Vue({
