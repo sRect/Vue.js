db6bcdce68944aa44140221591a4ec8f369cf833
sRect <srect2017@gmail.com>
2017-08-13 22:50:30 +0800

    更改url接口

--- vue2.0/financeCenter/src/main.js
+++ vue2.0/financeCenter/src/main.js
@@ -21,8 +21,8 @@ Vue.use(VueRouter)
 
 axios.defaults.timeout = 5000; //响应时间
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8'; //配置请求头
-axios.defaults.baseURL="http://192.168.1.30:8080";
-//axios.defaults.baseURL="http://www.ehaofangwang.com";
+// axios.defaults.baseURL="http://192.168.1.30:8080";
+axios.defaults.baseURL="http://www.ehaofangwang.com";
 Vue.prototype.$http=axios;
 
 let loadingInstance = null;
