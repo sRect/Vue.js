f421fdf1d1f2154045b67db78c50fcc0eac161f5
sRect <fcqun@sina.cn>
2017-08-17 13:56:38 +0800

    优化tab点击

--- vue2.0/vuex-demo3/src/components/HeaderVue.vue
+++ vue2.0/vuex-demo3/src/components/HeaderVue.vue
@@ -2,13 +2,13 @@
     <div id="header">
         <mt-navbar v-model="selected">                       
             <mt-tab-item id="1">
-                <router-link to="/home" tag="span">主页</router-link> 
+                <router-link to="/home" tag="p">主页</router-link> 
             </mt-tab-item>
             <mt-tab-item id="2">
-                <router-link to="/news" tag="span">新闻</router-link>
+                <router-link to="/news" tag="p">新闻</router-link>
             </mt-tab-item>
             <mt-tab-item id="3">
-                <router-link to="/about" tag="span">关于</router-link>
+                <router-link to="/about" tag="p">关于</router-link>
             </mt-tab-item>         
         </mt-navbar>
 
@@ -30,4 +30,11 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+    .mint-tab-item{
+        padding: 0;
+    }
+</style>
+
 
