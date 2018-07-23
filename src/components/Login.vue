b380e0cb3584ffd16f825a67681e7a4993428a0e
srect <fcqun@sina.cn>
2018-07-23 15:55:28 +0800

    :memo: 登录页添加备注信息

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,6 +3,11 @@
     <p>用户名：<input type="text" v-model="username" placeholder='请输入用户名...'></p>
     <p>密&nbsp;&nbsp;&nbsp;&nbsp;码：<input type="password" v-model="password" placeholder='请输入密码...'></p>
     <p><button @click="handleLogin">登录</button></p>
+    <hr>
+    <div>
+      <p>管理员权限：admin | 123</p>
+      <p>超级管理员： superadmin</p>
+    </div>
   </div>
 </template>
 
