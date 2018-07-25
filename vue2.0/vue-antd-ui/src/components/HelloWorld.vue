f46e45fb1fef1bc8b392287566e5dc74a899c8bc
srect <fcqun@sina.cn>
2018-07-25 18:10:51 +0800

    :lipstick: 使用vue-antd-ui组件

--- vue2.0/vue-antd-ui/src/components/HelloWorld.vue
+++ vue2.0/vue-antd-ui/src/components/HelloWorld.vue
@@ -1,6 +1,33 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <a-button type="primary" @click="showModal">Primary</a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">Dashed</a-button>
+    <a-button type="danger">Danger</a-button>
+
+    <div>
+      <a-button-group>
+        <a-button>Cancel</a-button>
+        <a-button type="primary">OK</a-button>
+      </a-button-group>
+    </div>
+
+    <div :style="{ width: '300px', border: '1px solid #d9d9d9', borderRadius: '4px' }">
+      <a-calendar :fullscreen="false" @panelChange="onPanelChange" />
+    </div>
+
+    <div>
+      <a-modal
+      title="提示"
+      :visible="visible"
+      cancelText="取消"
+      okText="确定"
+      @ok="handleOk"
+      :confirmLoading="confirmLoading"
+      @cancel="handleCancel">
+      <p>{{ModalText}}</p>
+    </a-modal>
+    </div>
   </div>
 </template>
 
@@ -9,7 +36,29 @@ export default {
   name: 'HelloWorld',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      ModalText: 'Content of the modal',
+      visible: false,
+      confirmLoading: false
+    }
+  },
+  methods: {
+    onPanelChange(value, mode) {
+      console.log(value, mode)
+    },
+    showModal() {
+      this.visible = true
+    },
+    handleOk(e) {
+      this.ModalText = 'The modal will be closed after two seconds'
+      this.confirmLoading = true
+      setTimeout(() => {
+        this.visible = false
+        this.confirmLoading = false
+      }, 2000)
+    },
+    handleCancel(e) {
+      console.log('Clicked cancel button')
+      this.visible = false
     }
   }
 }
--- vue2.0/vue-antd-ui/src/main.js
+++ vue2.0/vue-antd-ui/src/main.js
@@ -4,7 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import { Button, Calendar, Modal } from 'vue-antd-ui'
+
+import moment from 'moment'
+import 'moment/locale/zh-cn'
+moment.locale('zh-cn')
+
 Vue.config.productionTip = false
+
+Vue.component(Button.name, Button)
+Vue.component(Button.Group.name, Button.Group)
+Vue.component(Calendar.name, Calendar)
+Vue.component(Modal.name, Modal)
 
 /* eslint-disable no-new */
 new Vue({
